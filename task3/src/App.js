92d77903397caefdae32590c7da4487d8384de63
Janch <suslik22881@gmail.com>
2023-01-16 03:44:29 +0200

    sas

--- task3/src/App.js
+++ task3/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './modules/HeaderHome';
-import Footer from './modules/Footer';
 import Home from './modules/pages/Home';
 import PaletteRus from './modules/pages/PalettePages/rus/PaletteRus';
 import PaletteAus from './modules/pages/PalettePages/aus/PaletteAus';
